138fb1babfa79d7d8f8f28a1ee3796d70c44b7fc
JavaScriptnoob1 <script1314java@gmail.com>
2017-08-09 00:32:39 +0800

    add webpack-dev-middleware-for-koa2

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
 import webpack from 'webpack';
 import Koa from 'koa';
 import opn from 'opn';
-import { hotMiddleware } from 'koa-webpack-middleware';
+import { koaDevMiddleware, koaHotMiddleware } from 'webpack-dev-middleware-for-koa2';
 import devConfig from '../webpack.config.dev';
 import { port } from '../config/server';
-import devMiddleware from './utils/webpackUtils';
 
 const compile = webpack(devConfig);
 
 const app = new Koa();
-const devMiddlewareCOmpile = devMiddleware(compile, devConfig.devServer, () => {
+const devMiddlewareCompile = koaDevMiddleware(compile, devConfig.devServer, () => {
   app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
     const uri = `http://localhost:${port}`;
     opn(uri);
   });
 });
-app.use(devMiddlewareCOmpile);
-app.use(hotMiddleware(compile, {
+app.use(devMiddlewareCompile);
+app.use(koaHotMiddleware(compile, {
   path: '/__webpack_hmr',
   heartbeat: 10 * 1000,
   log: console.log,
